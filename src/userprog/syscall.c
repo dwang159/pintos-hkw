2d276ec4e8306695b84f9971be58c335bf23fd54
Daniel Kong <dkong1796@gmail.com>
2014-02-16 13:38:04 -0800

    Can read syscall number.
    - Put a lot of print statements to see where syscalls are being called.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -17,20 +17,47 @@ static void syscall_handler(struct intr_frame *f) {
     switch (syscall_nr)
     {
     case SYS_HALT:
+        printf("SYS_HALT\n");
+        break;
     case SYS_EXIT:
+        printf("SYS_EXIT\n");
+        break;
     case SYS_EXEC:
+        printf("SYS_EXEC\n");
+        break;
     case SYS_WAIT:
+        printf("SYS_WAIT\n");
+        break;
     case SYS_CREATE:
+        printf("SYS_CREATE\n");
+        break;
     case SYS_REMOVE:
+        printf("SYS_REMOVE\n");
+        break;
     case SYS_OPEN:
+        printf("SYS_OPEN\n");
+        break;
     case SYS_FILESIZE:
+        printf("SYS_FILESIZE\n");
+        break;
     case SYS_READ:
+        printf("SYS_READ\n");
+        break;
     case SYS_WRITE:
+        printf("SYS_WRITE\n");
+        break;
     case SYS_SEEK:
+        printf("SYS_SEEK\n");
+        break;
     case SYS_TELL:
+        printf("SYS_TELL\n");
+        break;
     case SYS_CLOSE:
+        printf("SYS_CLOSE\n");
+        break;
+    default:
+        printf("INVALID\n");
         break;
     }
-    printf("system call: %d\n", syscall_nr);
     thread_exit();
 }
