8c054521e1906a5949a3df2900aec8d97d7f2da9
Daniel Kong <dkong1796@gmail.com>
2014-02-15 22:15:46 -0800

    Fixed bugs in passing args
    - Did some basic testing usint printf to check what's being pushed onto
      stack.
    - Fixed several bugs causing garbage to be written.
    - Can't run programs yet because user memory access isn't working.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -10,8 +10,27 @@ void syscall_init(void) {
     intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static void syscall_handler(struct intr_frame *f UNUSED) {
-    printf("system call!\n");
+static void syscall_handler(struct intr_frame *f) {
+    void *esp = f->esp;
+    int syscall_nr = *((int *) esp);
+
+    switch (syscall_nr)
+    {
+    case SYS_HALT:
+    case SYS_EXIT:
+    case SYS_EXEC:
+    case SYS_WAIT:
+    case SYS_CREATE:
+    case SYS_REMOVE:
+    case SYS_OPEN:
+    case SYS_FILESIZE:
+    case SYS_READ:
+    case SYS_WRITE:
+    case SYS_SEEK:
+    case SYS_TELL:
+    case SYS_CLOSE:
+        break;
+    }
+    printf("system call: %d\n", syscall_nr);
     thread_exit();
 }
-
