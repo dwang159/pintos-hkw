78412e38ca89cfd388a3f7c85e6b3edef8af1145
Daniel Wang <dwang159@gmail.com>
2014-02-16 23:45:43 -0800

    Implemented some syscall infrastructure
    Also, released locks in thread.c when exiting threads.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,23 +3,25 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+#include "pagedir.c"
 
 static void syscall_handler(struct intr_frame *);
 
 /* System calls. */
-void sys_halt(void);
-void sys_exit(int status);
-pid_t sys_exec(const char *cmd_line);
-int sys_wait(pid_t pid);
-bool sys_create(const char *file, unsigned int initial_size);
-bool sys_remove(const char *file);
-int sys_open(const char *file);
-int sys_filesize(int fd);
-int sys_read(int fd, void *buffer, unsigned int size);
-int sys_write(int fd, const void *buffer, unsigned int size);
-void sys_seek(int fd, unsigned int position);
-unsigned int sys_tell(int fd);
-void sys_close(int fd);
+void sys_halt(const void **args);
+void sys_exit(const void **args);
+tid_t sys_exec(const void **args);
+int sys_wait(const void **args);
+bool sys_create(const void **args);
+bool sys_remove(const void **args);
+int sys_open(const void **args);
+int sys_filesize(const void **args);
+int sys_read(const void **args);
+int sys_write(const void **args);
+void sys_seek(const void **args);
+unsigned int sys_tell(const void **args);
+void sys_close(const void **args);
 
 void syscall_init(void) {
     intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
@@ -27,52 +29,81 @@ void syscall_init(void) {
 
 static void syscall_handler(struct intr_frame *f) {
     void *esp = f->esp;
+
+    /* Check pointer validity */
+    if (!mem_valid(esp)) {
+        thread_exit();
+        return;
+    }
+
     int syscall_nr = *((int *) esp);
+    esp += sizeof(int);
 
     switch (syscall_nr)
     {
     case SYS_HALT:
         printf("SYS_HALT\n");
+        sys_halt(esp);
         break;
     case SYS_EXIT:
         printf("SYS_EXIT\n");
+        sys_exit(esp);
         break;
     case SYS_EXEC:
         printf("SYS_EXEC\n");
+        sys_exec(esp);
         break;
     case SYS_WAIT:
         printf("SYS_WAIT\n");
+        sys_wait(esp);
         break;
     case SYS_CREATE:
         printf("SYS_CREATE\n");
+        sys_create(esp);
         break;
     case SYS_REMOVE:
         printf("SYS_REMOVE\n");
+        sys_remove(esp);
         break;
     case SYS_OPEN:
         printf("SYS_OPEN\n");
+        sys_open(esp);
         break;
     case SYS_FILESIZE:
         printf("SYS_FILESIZE\n");
+        sys_filesize(esp);
         break;
     case SYS_READ:
         printf("SYS_READ\n");
+        sys_read(esp);
         break;
     case SYS_WRITE:
         printf("SYS_WRITE\n");
+        sys_write(esp);
         break;
     case SYS_SEEK:
         printf("SYS_SEEK\n");
+        sys_seek(esp);
         break;
     case SYS_TELL:
         printf("SYS_TELL\n");
+        sys_tell(esp);
         break;
     case SYS_CLOSE:
         printf("SYS_CLOSE\n");
+        sys_close(esp);
         break;
     default:
         printf("INVALID\n");
+        thread_exit();
         break;
     }
-    thread_exit();
+    return;
+}
+
+/* Checks that addr points into user space on a currently mapped page */
+bool mem_valid(const void *addr)
+{
+    return (is_user_vaddr(addr) &&
+                    lookup_page(thread_current()->pagedire, addr, false) != NULL);
 }
