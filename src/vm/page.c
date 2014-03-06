9e29aed175dd85108f0debf807c4dce49305e9e0
Daniel Kong <dkong1796@gmail.com>
2014-03-06 12:35:15 -0800

    Progress on basic interface for page table
    - Functions for working with the hash table implemented.

--- src/vm/page.c
+++ src/vm/page.c
@@ -9,10 +9,6 @@
 #include "threads/pte.h"
 #include "threads/malloc.h"
 
-unsigned spt_hash(void *uaddr);
-unsigned spt_get_key(void *uaddr);
-bool spt_entry_cmp(struct spt_entry *e1, struct spt_entry *e2);
-
 /* Create a new page table. Returns a null pointer if memory
  * allocation fails.
  */
@@ -28,25 +24,35 @@ struct spt_table *spt_create_table() {
 /* Create a new page table entry. Returns a null pointer if
  * memory allocation fails.
  */
-struct spt_entry *spt_create_entry() {
+struct spt_entry *spt_create_entry(unsigned key) {
     struct spt_entry *spte;
 
     spte = (struct spt_entry *) malloc(sizeof(struct spt_entry));
     if (!spte)
         return NULL;
+
+    spte->key = key;
     return spte;
 }
 
 /* Look up a page table entry. */
 struct spt_entry *spt_lookup(struct spt_table *spt, void *uaddr) {
     struct hash_elem *e;
+    struct spt_entry *cmp;
 
-    // TODO
+    // Create a temporary struct with the same key so we can find
+    // an "equal" element in the hash table.
+    cmp = spt_create_entry(spt_get_key(uaddr));
+    if (!cmp)
+        return NULL;
+    e = hash_find(spt, &cmp->elem);
+    free(cmp);
+
+    return hash_entry(e, struct spt_entry, elem);
 }
 
-
 /* Hashes a pointer. */
-unsigned spt_hash(hash_elem *e) {
+unsigned spt_hash(hash_elem *e, void *aux UNUSED) {
     struct spt_entry *spte = hash_entry(e, struct spt_entry, elem);
     return hash_int((int) spt_get_key(spte->page_uaddr));
 }
@@ -57,4 +63,14 @@ unsigned spt_hash(hash_elem *e) {
  */
 unsigned spt_get_key(void *uaddr) {
     return uaddr & (PDMASK | PTMASK);
+}
+
+/* Compares two hash elems. Returns true if e1 < e2. */
+bool spt_entry_cmp(struct spt_entry *e1, struct spt_entry *e2, void *aux) {
+    struct spt_entry *spte1, *spte2;
+
+    spte1 = hash_entry(e1, struct spt_entry, elem);
+    spte2 = hash_entry(e2, struct spt_entry, elem);
+
+    return spte1->key < spte2->key;
 }
--- src/vm/page.h
+++ src/vm/page.h
@@ -19,18 +19,27 @@ struct spt_entry {
   // Element used to store entry in page table.
   struct hash_elem elem;
 
-  // Virtual address of start of page. Used to identify page.
-  void *page_uaddr;
+  // Key used to identify page. Should be the uaddr of the page with
+  // the lower 12 bits zeroed out.
+  unsigned key;
 };
 
 /* Create a new page table. */
 struct spt_table *spt_create_table(void);
 
 /* Create a new page table entry. */
-struct spt_entry *spt_create_entry(void);
+struct spt_entry *spt_create_entry(void *uaddr);
 
 /* Look up a page table entry. */
 struct spt_entry *spt_lookup(struct spt_table *spt, void *uaddr);
 
+/* Returns a key to look up an entry in the hash table given a uaddr. */
+unsigned spt_get_key(void *uaddr);
+
+/* Returns true if e1 < e2. */
+bool spt_entry_cmp(struct spt_entry *e1, struct spt_entry *e2, void *aux);
+
+/* Hash function for the supplemental page table. */
+unsigned spt_hash(hash_elem *e, void *aux);
 
 #endif /* VM_PAGE_H */
