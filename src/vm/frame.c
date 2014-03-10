298e99531e38f02e342eab5cadc905e56bfae86c
Tim Holland <th0114nd@gmail.com>
2014-03-09 18:00:59 -0700

    Frame compiles!
    but it's still missing a lot of features.

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -1,12 +1,19 @@
 #include "vm/frame.h"
-#include <time.h>
+#include <devices/timer.h>
+#include <lib/random.h>
 #include "threads/pte.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+typedef uint32_t flags_t;
+
+hash_hash_func frame_hash;
+hash_less_func frame_less;
 
 /* Takes the hash of the frame number. */
 unsigned frame_hash(const struct hash_elem *p_, void *aux UNUSED) {
     const struct frame *f = hash_entry(p_, struct frame, hash_elem);
-    return hash_int(frame->frame_no);
+    return hash_int(f->frame_no);
 }
 
 bool frame_less(const struct hash_elem *a_, const struct hash_elem *b_,
@@ -16,6 +23,8 @@ bool frame_less(const struct hash_elem *a_, const struct hash_elem *b_,
     return a->frame_no < b->frame_no;
 }
 
+/* Initializes the frame table for the process that has 
+ * just started. */
 
 void frame_table_init() {
     list_init(&ft_list);
@@ -26,35 +35,83 @@ void frame_table_init() {
 }
 
 void frame_table_destroy() {
-    iock_acquire(&ft_lock);
+    lock_acquire(&ft_lock);
     /* TODO: Be a bit more careful about freeing resources? */
-    hash_destroy(ft_hash, NULL);
+    hash_destroy(&ft_hash, NULL);
 }
 
-/* Returns an array to the ptes associated with the frame number. */
-uint32_t *frame_table_lookup(uint32_t frame_no) {
-    struct frame f;
+/* Returns an array to the pageinfos associated with the frame number. */
+pageinfo *frame_table_lookup(uint32_t frame_no) {
     struct frame *fp;
-    struct hash_elem *he;
-    lock_aquire(&ft_lock);
-    he = hash_find(&ft_hash, &f.hash_elem);
-    if (he != NULL) {
-        fp = hash_entry(he, struct frame, hash_elem); 
-        lock_release(&ft_lock);
-        return fp->pages;
+    fp = find_frame(frame_no);
+    if (fp != NULL) {
+        return fp->ptes;
     } else {
         return NULL; 
     }
 }
 
-/* Eviction policies. */
-struct frame *random_frame(const uint32_t *pagedir) {
-    srand(time(NULL));
-    uint32_t num = rand() % NUM_FRAMES;
-    while (!VALID_FRAME_NO(num)) {
-        num = rand() % NUM_FRAMES;
+struct frame *find_frame(uint32_t frame_no) {
+    struct frame f;
+    struct frame *fp;
+    struct hash_elem *he;
+    lock_acquire(&ft_lock);
+    f.frame_no = frame_no;
+    he = hash_find(&ft_hash, &f.hash_elem);
+    if (he != NULL) {
+        fp = hash_entry(he, struct frame, hash_elem);
+        lock_release(&ft_lock);
+        return fp;
+    } else {
+        return NULL;
     }
-    return frame_table_lookup
+}
+
+/* Adds or removes a page to the array associated with that frame in
+ * the frame table. */
+void frame_table_add_vpage(uint32_t frame_no, uint32_t page_no) {
+    printf("Attempted to add page %u from frame %u\n", page_no,
+                                                          frame_no);
+    printf("Error: not implemented\n");
+}
+
+void frame_table_rem_vpage(uint32_t frame_no, uint32_t page_no) {
+    printf("Attempted to remove page %u from frame %u\n", page_no,
+                                                          frame_no);
+    printf("Error: not implemented\n");
+}
+
+void frame_table_set_flags(uint32_t frame_no, flags_t flags) {
+    printf("frame_table_set_flags called with %u\n", frame_no);
+    printf("Attempted flags were %d\n", flags);
+    printf("Error: not implemented\n");
+}
+
+flags_t frame_table_get_flags(uint32_t frame_no) {
+    printf("frame_table_get_flags called with %u\n", frame_no);
+    printf("Error: not implemented\n");
+    return -1;
+}
+
+/* Evicts the frame decided by the policy pol, and returns that
+ * frame number. Needs to clear the entry in the frame table,
+ * the page table, move the contents into swap or write it back to
+ * the file that it belongs to. */
+uint32_t frame_evict(policy_t pol) {
+    printf("frame_evict called with policy %p\n", pol);
+    printf("But not implemented...\n");
+    return -1;
+}
+
+/* Eviction policies. */
+struct frame *random_frame() {
+    random_init((unsigned) timer_ticks());
+    uint32_t num;
+    num = (uint32_t) random_ulong() % NUM_FRAMES;
+    while (!VALID_FRAME_NO(num)) {
+        num = (uint32_t) random_ulong() % NUM_FRAMES;
+    }
+    return find_frame(num);
 }
 
 struct frame *lru_frame() {
@@ -63,8 +120,8 @@ struct frame *lru_frame() {
     for(e = list_begin(&ft_list); 
           e != list_end(&ft_list);
           e = list_next(e)) {
-        struct frame *f = list_entry(e, frame_t, elem);
-        page pages_for_f[PAGE_FRAME_RATIO] =  f->ptes;
+        struct frame *f = list_entry(e, struct frame, list_elem);
+        pageinfo *pages_for_f =  f->ptes;
         /* Iterate through the pages that map to the frame f,
          * and say that the frame is dirty if any page is
          * and accessed if any page is. */
@@ -73,9 +130,9 @@ struct frame *lru_frame() {
         int i;
         uint32_t pte;
         for (i = 0; i < PAGE_FRAME_RATIO; i++) {
-            pte = page_for_f[i];
-            accessed &&= (p & PTE_A);
-            dirty &&= (p & PTE_D);
+            pte = pages_for_f[i].pte;
+            accessed = accessed && (pte & PTE_A);
+            dirty = dirty && (pte & PTE_D);
         }
         if (!accessed)
             continue; /* Don't evict a frame not used yet. */
@@ -83,6 +140,6 @@ struct frame *lru_frame() {
         lock_release(&ft_lock);
         return f;
     }
-    lock_release_(&ft_lock);
+    lock_release(&ft_lock);
     return NULL;
 }
--- src/vm/frame.h
+++ src/vm/frame.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 #include "kernel/list.h"
+#include "threads/thread.h"
 #include "lib/kernel/hash.h"
 #define NUM_FRAMES (1 << 20) 
 #define PAGE_FRAME_RATIO 1
@@ -11,7 +12,12 @@
 /* A policy should not mutate, and it should only need to consult a
  * frame table to see which frames are older, and a page table
  * to examine dirty and accessed bits. */
-typedef struct frame *policy_t(const uint32_t *);
+typedef struct frame *policy_t(void);
+
+typedef struct _pageinfo {
+    uint32_t pte;
+    pid_t owner;
+} pageinfo;
 
 /* The structure corresponding to a frame table entry.
  * Records its position in ft_hash, ft_list, as well as
@@ -21,43 +27,33 @@ struct frame {
     struct list_elem list_elem;
     uint32_t frame_no;
     bool dirty;
-    struct {uint32_t pte, pid_t owner} ptes[PAGE_FRAME_RATIO];
+    pageinfo ptes[PAGE_FRAME_RATIO];
 };
 
 struct list ft_list;
 struct hash ft_hash;
 struct lock ft_lock;
 
-/* Initializes the frame table for the process that has 
- * just started. */
 void frame_table_init(void);
 void frame_table_destroy(void);
 
-/* Adds or removes a page to the array associated with that frame in
- * the frame table. */
 void frame_table_add_vpage(uint32_t frame_no, uint32_t page_no);
 void frame_table_rem_vpage(uint32_t frame_no, uint32_t page_no);
 
-/* Returns the pte's associated with the frame as a null
- * terminated array. */
-uint32_t *frame_table_get_ptes(uint32_t frame_no);
+struct frame *find_frame(uint32_t frame_no);
+pageinfo *frame_table_lookup(uint32_t frame_no);
 
-/* Mutators and accessors for the flags corresponding to frm in ft. */
-void frame_table_set_flags(frame_table_t ft, frame_t frm, flags_t flags);
-flags_t frame_table_get_flags(frame_t frm);
+void frame_table_set_flags(uint32_t frame_no, uint32_t flags);
+uint32_t frame_table_get_flags(uint32_t frame_no);
 
 /* Evicts the frame decided by the policy pol, and returns that
  * frame number. Needs to clear the entry in the frame table,
  * the page table, move the contents into swap or write it back to
  * the file that it belongs to. */
-frame_t frame_evict(policy_t pol);
+uint32_t frame_evict(policy_t pol);
 
 /* Frees all frames associated with the exiting process. */
 void frame_free_all(void);
-
-/* Mutators and accessors for the flags corresponding to frm in ft. */
-void frame_table_set_flags(frame_table_t ft, frame_t frm, flags_t flags);
-flags_t frame_table_get_flags(frame_t frm);
 
 /* Adds or removes a page to the array associated with that frame in
  * the frame table. */
