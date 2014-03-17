7d345cadab81c2e097fc6bf20af76640320a3b43
Tim Holland <th0114nd@gmail.com>
2014-03-16 23:54:58 -0700

    Pretty sure I have the API I want for the filesystem cache
    Basically the way it works is by wrapping the calls that inode.c
    was doing to block_read and block_write. If it controls all the
    access to the file system block device, then it can manage when
    writes to the disk actually happen and be confident that it knows
    what's up. Now I just need to figure out internals.
    Passes the filesystem tests, except for occasionally syn-read and
    syn-write

--- src/filesys/cache.c
+++ src/filesys/cache.c
@@ -1,11 +1,33 @@
 #include "filesys/cache.h"
 #include "filesys/filesys.h"
 #include "devices/block.h"
+#include "threads/malloc.h"
+#include <string.h>
 
-void cache_read(block_sector_t sect, void *addr) {
-    block_read(fs_device, sect, addr);
+static char bounce[BLOCK_SECTOR_SIZE];
+
+/* Reads the filesys sector sect into addr. Starts at offset into the
+ * sector and reads size bytes. This is a read of at most *one* sector.
+ * Will consult the cache and only go to the disk if necessary. */
+void cache_read(block_sector_t sect, void *addr, off_t offset, off_t size) {
+    ASSERT(offset >= 0);
+    ASSERT(size >= 0);
+    ASSERT(size + offset <= BLOCK_SECTOR_SIZE);
+    block_read(fs_device, sect, bounce);
+    memcpy(addr, bounce + offset, size);
 }
 
-void cache_write(block_sector_t sect, const void *addr) {
-    block_write(fs_device, sect, addr);
+void cache_write(block_sector_t sect, const void *addr, off_t offset,
+        off_t size) {
+    ASSERT(offset >= 0);
+    ASSERT(size >= 0);
+    ASSERT(size + offset <= BLOCK_SECTOR_SIZE);
+    void *bounce = malloc(BLOCK_SECTOR_SIZE);
+    if (offset > 0 || offset + size < BLOCK_SECTOR_SIZE) {
+        block_read(fs_device, sect, bounce);
+    } else {
+        memset(bounce, 0, BLOCK_SECTOR_SIZE);
+    }
+    memcpy(bounce + offset, addr, size);
+    block_write(fs_device, sect, bounce);
 }
--- src/filesys/cache.h
+++ src/filesys/cache.h
@@ -2,9 +2,11 @@
 #define FILESYS_CACHE_H
 
 #include "devices/block.h"
+#include "filesys/off_t.h"
 
 /* Read from the buffer the block_sector into the pointer */
-void cache_read(block_sector_t sect, void *target);
-void cache_write(block_sector_t sect, const void *source);
+void cache_read(block_sector_t sect, void *target, off_t start, off_t size);
+void cache_write(block_sector_t sect, const void *source, off_t start,
+        off_t size);
 
 #endif /* FILESYS_CACHE_H */
